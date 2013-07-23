b0768098045ac85b1b27de03f9460c5948daa243
Joe Friedl <joe@joefriedl.net>
2013-07-23 16:33:17 -0400

    Show posted data, passing all tests

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -5,12 +5,33 @@ require('should');
 var sinon = require('sinon');
 var controllers = require('../../wassup/controllers');
 
+
+var FakeResponse = function() {
+};
+
+FakeResponse.prototype.render = function(view, data) {
+  this.view = view;
+  this.data = data;
+};
+
+
 describe('index', function() {
   it('should render the index view', function() {
-    var req = {}, res = {'render': sinon.spy()};
+    var req = {};
+    var res = new FakeResponse();
 
     controllers.index(req, res);
 
-    res.render.calledWith('index').should.be.true;
+    res.view.should.equal('index');
+  });
+
+  it('should display POSTed data', function() {
+    var req = {body: {new_url: 'http://google.com'}};
+    var res = new FakeResponse();
+
+    controllers.index(req, res);
+
+    res.view.should.eql('index')
+    res.data.should.eql({new_url: 'http://google.com'});
   });
 });
--- wassup/controllers.js
+++ wassup/controllers.js
@@ -2,7 +2,13 @@
  * Controllers
  */
 var index = function(req, res) {
-  return res.render('index');
+  var data = {};
+
+  if (req.body) {
+    data.new_url = req.body.new_url;
+  }
+
+  return res.render('index', data);
 };
 
 module.exports.index = exports.index = index;
