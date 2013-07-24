5c11a0a94dfac361d60f312ddc3d6f945f7bae70
Joe Friedl <joe@joefriedl.net>
2013-07-24 11:28:16 -0400

    Spec out a functional test for showing server status

---
+++ test/functional/ping.js
@@ -0,0 +1,26 @@
+/*
+ * Functional tests for pinging hosts
+ */
+describe('pings', function() {
+  it('should show whether a URL is up or down', function(done) {
+    // The user goes to the home page
+
+    // They add a URL that's expected to be up
+
+    // And see it on the page, marked as "up"
+
+    // Next, they add a URL that's not expected to be up
+
+    // They see it on the page, marked as "down"
+
+    // The first URL goes down!
+
+    // It's now marked as "down"
+
+    // The second URL comes up!
+
+    // It's now marked as "up"
+
+    throw new Error('Finish the test!');
+  });
+});
