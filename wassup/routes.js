11e67bc8732c03bf0f8d8cba5bf32f5a69d4b6cc
Joe Friedl <joe@joefriedl.net>
2013-07-24 00:19:09 -0400

    Even more REST

--- wassup/routes.js
+++ wassup/routes.js
@@ -6,6 +6,6 @@ var controllers = require('./controllers');
 // Export a function that takes an app and binds controllers to routes
 module.exports = exports = function(app) {
   app.get('/', controllers.index);
-  app.post('/', controllers.index);
+  app.post('/urls/', controllers.index);
   app.delete(/^\/urls\/(.*)/, controllers.delete_url);
 };
