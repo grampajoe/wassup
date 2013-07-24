814d43fd737a997842ce407684c3b4f8d21c4fb7
Joe Friedl <joe@joefriedl.net>
2013-07-24 14:27:35 -0400

    Ping URLs and show their status on submit, try to make testing responses nicer

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -3,23 +3,63 @@
  */
 require('should');
 var sinon = require('sinon');
+var nock = require('nock');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 var controllers = require('../../wassup/controllers');
 
 
 var FakeResponse = function() {
+  EventEmitter.call(this);
+
+  this.ended = false;
+
+  this.on('end', function() {
+    this.ended = true;
+  });
+};
+
+util.inherits(FakeResponse, EventEmitter);
+
+// Make the response work kind of like a promise
+FakeResponse.prototype.then = function(callback) {
+  var self = this;
+
+  if (this.ended) {
+    callback();
+  } else {
+    this.on('end', function listener() {
+      self.removeListener('end', listener);
+
+      callback();
+    });
+  }
 };
 
 FakeResponse.prototype.render = function(view, data) {
   this.view = view;
   this.data = data;
+
+  this.emit('end');
 };
 
-FakeResponse.prototype.redirect = sinon.spy();
+FakeResponse.prototype.redirect = function(url) {
+  this.location = url;
+
+  this.emit('end');
+};
 
 
 describe('index', function() {
+  beforeEach(function() {
+    nock('http://up.com').get('/').reply(200, 'OK');
+    nock('http://google.com').get('/').reply(200, 'OK');
+    nock('http://fark.com').get('/').reply(200, 'OK');
+  });
+
   afterEach(function() {
     controllers.urls = [];
+    nock.cleanAll();
   });
 
   it('should render the index view', function() {
@@ -31,39 +71,89 @@ describe('index', function() {
     res.view.should.equal('index');
   });
 
-  it('should display POSTed data', function() {
-    var req = {body: {new_url: 'http://google.com'}};
+  it('should display data', function() {
+    var req = {};
     var res = new FakeResponse();
+    controllers.urls = [
+      {'url': 'http://google.com', 'status': 'up'},
+    ];
 
     controllers.index(req, res);
 
     res.view.should.eql('index')
-    res.data.should.eql({urls: ['http://google.com']});
+    res.data.should.eql({urls: [
+      {'url': 'http://google.com', 'status': 'up'},
+    ]});
   });
 
-  it('should save POSTed data', function() {
+  it('should save POSTed data', function(done) {
     var req = {body: {}};
     var res = new FakeResponse();
 
     req.body.new_url = 'http://google.com';
     controllers.index(req, res);
 
-    res.data.should.eql({urls: ['http://google.com']});
+    res.then(function() {
+      controllers.urls.should.eql([
+        {'url': 'http://google.com', 'status': 'up'},
+      ]);
 
-    req.body.new_url = 'http://fark.com';
-    controllers.index(req, res);
+      res.location.should.eql('/');
 
-    res.data.should.eql({urls: ['http://google.com', 'http://fark.com']});
-    res.redirect.calledWith('/').should.be.true;
+      req.body.new_url = 'http://fark.com';
+      res = new FakeResponse();
+      controllers.index(req, res);
+
+      res.then(function() {
+        controllers.urls.should.eql([
+          {'url': 'http://google.com', 'status': 'up'},
+          {'url': 'http://fark.com', 'status': 'up'},
+        ]);
+
+        res.location.should.eql('/');
+
+        done();
+      });
+    });
   });
 
-  it('should only save when something actually gets POSTed', function() {
+  it('should only save when something actually gets POSTed', function(done) {
     var req = {body: {}};
     var res = new FakeResponse();
 
     controllers.index(req, res);
 
-    res.data.should.eql({urls: []});
+    res.then(function() {
+      controllers.urls.should.eql([]);
+
+      done();
+    });
+  });
+
+  it('should ping URLS when they are added', function() {
+    var req = {body: {}};
+    var res = new FakeResponse();
+
+    req.body.new_url = 'http://up.com';
+    controllers.index(req, res);
+
+    res.then(function() {
+      controllers.urls.should.eql([
+        {'url': 'http://up.com', 'status': 'up'},
+      ]);
+
+      // Try a URL that won't return a response
+      controllers.urls = [];
+      req.body.new_url = 'http://what-if-this-was-real.fake.com';
+      res = new FakeResponse();
+      controllers.index(req, res);
+
+      res.then(function() {
+        controllers.urls.should.eql([
+          {'url': 'http://what-if-this-was-real.fake.com', 'status': 'down'},
+        ]);
+      });
+    });
   });
 });
 
@@ -75,11 +165,17 @@ describe('delete_url', function() {
   it('should delete a given URL', function() {
     var req = {params: ['http://google.com']};
     var res = new FakeResponse();
-    controllers.urls = ['http://google.com', 'http://fark.com'];
+    controllers.urls = [
+      {'url': 'http://google.com', 'status': 'up'},
+      {'url': 'http://fark.com', 'status': 'up'},
+    ];
 
     controllers.delete_url(req, res);
 
-    controllers.urls.should.eql(['http://fark.com']);
-    res.redirect.calledWith('/').should.be.true;
+    controllers.urls.should.eql([
+      {'url': 'http://fark.com', 'status': 'up'},
+    ]);
+
+    res.location.should.eql('/');
   });
 });
--- wassup/controllers.js
+++ wassup/controllers.js
@@ -1,23 +1,41 @@
 /*
  * Controllers
  */
+var request = require('request');
+
+
 exports.urls = [];
 
 exports.index = function(req, res) {
   if (req.body && 'new_url' in req.body) {
-    exports.urls.push(req.body.new_url);
+    var url = {
+      'url': req.body.new_url,
+      'status': 'down',
+    };
 
-    res.redirect('/');
+    exports.urls.push(url);
+
+    request(url.url, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        url.status = 'up';
+      }
+
+      res.redirect('/');
+    });
+  } else {
+    res.render('index', {urls: exports.urls});
   }
-
-  res.render('index', {urls: exports.urls});
 };
 
 exports.delete_url = function(req, res) {
   var url = req.params[0];
 
-  var i = exports.urls.indexOf(url);
-  exports.urls.splice(i, 1);
+  for (var i = 0; i < exports.urls.length; i++) {
+    if (exports.urls[i].url == url) {
+      exports.urls.splice(i, 1);
+      break;
+    }
+  }
 
   res.redirect('/');
 };
